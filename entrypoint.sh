1f81d1087b798bf7708187f1036818b6246becc1
rwekwe <129355294+rwekwe@users.noreply.github.com>
2023-10-26 04:36:02 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="8b39aaf0-d13e-45f9-8848-1d736b51db28"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
